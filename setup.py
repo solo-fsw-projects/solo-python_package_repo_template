c55e28d82cfdda2fc72042cf1b0b1623474a74a7
Jens van Bijsterveld <112897179+JensVanBijs@users.noreply.github.com>
2024-01-25 11:11:58 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='PACKAGE_NAME',  #TODO
+    url='',  #TODO
+    author='SOLO FSW',
+    author_email='labsupport@FSW.leidenuniv.nl',
+    packages=['PACKAGE_NAME'],
+    install_requires=[
+        ''  #TODO
+    ],
+    version='0.1',  #TODO
+    license='GPLv3',
+    description='',  #TODO
+    long_description=open('README.md').read(),
+)
